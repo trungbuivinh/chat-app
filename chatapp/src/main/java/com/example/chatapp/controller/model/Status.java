601a62fe454223d583606034000b66d376b53cf9
TrungBui <buivinhtrungqng@gmail.com>
2023-07-11 21:07:49 +0700

    remove reactjs

--- chatapp/src/main/java/com/example/chatapp/controller/model/Status.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatapp.controller.model;
-
-public enum Status {
-    JOIN,
-    MESSAGE,
-    LEAVE
-}
---
+++ chatserver/src/main/java/com/chatapp/chatserver/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.chatapp.chatserver.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
