99157e65895ab8caabf1ccc94a340aaa6956e404
TrungBui <buivinhtrungqng@gmail.com>
2023-08-15 23:02:44 +0700

    rev

--- chatserver/src/main/java/com/chatapp/chatserver/config/WebSocketConfig.java
+++ chatserver/src/main/java/com/chatapp/chatserver/config/WebSocketConfig.java
@@ -2,10 +2,11 @@ package com.chatapp.chatserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
+@CrossOrigin(origins = "*")
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
