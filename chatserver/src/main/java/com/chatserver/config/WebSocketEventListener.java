df352356e7c11d678e0a889d0e4803d1c8f46c26
TrungBui <buivinhtrungqng@gmail.com>
2023-08-16 00:27:25 +0700

    revert changes

---
+++ chatserver/src/main/java/com/chatserver/config/WebSocketEventListener.java
@@ -0,0 +1,48 @@
+package com.chatserver.config;
+
+import com.chatserver.model.Message;
+import com.chatserver.model.Status;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    //inject dependency
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = Message.builder()
+                    .status(Status.LEAVE)
+                    .senderName(username)
+                    .build();
+            messageTemplate.convertAndSend("/chatroom/public", chatMessage);
+        }
+    }
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User connected: {}", username);
+            var chatMessage = Message.builder()
+                    .status(Status.JOIN)
+                    .senderName(username)
+                    .build();
+            messageTemplate.convertAndSend("/chatroom/public", chatMessage);
+        }
+    }
+}
---
+++ chatserver/src/main/java/com/chatserver/model/Status.java
@@ -0,0 +1,7 @@
+package com.chatserver.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
